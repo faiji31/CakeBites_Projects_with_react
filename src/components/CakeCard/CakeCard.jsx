93c98972930b4d9ca43930288dec1b34d7c18cb8
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2026-01-28 21:39:37 +0600

    10th commit

--- src/components/CakeCard/CakeCard.jsx
+++ src/components/CakeCard/CakeCard.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const CakeCard = ({ cake }) => {
+
+  const handlecake =()=>{
+    console.log("clicked")
+  }
   return (
     <div>
      <div key={cake.id} className="group card bg-base-100 w-96 shadow-xl hover:shadow-2xl transition-all duration-300 border border-base-200 overflow-hidden">
@@ -29,7 +33,7 @@ const CakeCard = ({ cake }) => {
     </p>
 
     <div className="card-actions justify-end mt-4">
-      <button className="btn btn-primary btn-block group-hover:scale-[1.02] transition-transform">
+      <button onClick={handlecake} className="btn btn-primary btn-block group-hover:scale-[1.02] transition-transform">
         Add to Cart
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
--- src/components/SelectedCake/SelectedCake.jsx
+++ src/components/SelectedCake/SelectedCake.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SelectedCake = () => {
       return (
             <div className='max-w-7xl mx-auto mt-10'>
-                  <h2 className='text-2xl font-semibold'>Selected Cake</h2>
+                  
             </div>
       );
 };
